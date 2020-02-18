f38c5da21ff9778f60718622efb9c650499c945c
HaoXWei <1804659249@qq.com>
2020-02-18 21:30:26 +0800

    test

--- qianfeng_piao/settings.py
+++ qianfeng_piao/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'qianfeng_piao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'axf',
+        'HOST':'hxw1112.tk',
+        'PORT':3306,
+        'USER':'root',
+        'PASSWORD':'root'
     }
 }
 
